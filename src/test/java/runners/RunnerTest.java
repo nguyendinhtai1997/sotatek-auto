e705f5f874434cbfcd15efa91ee56dfc34686235
Nguyen Dinh Tai <taind5@tcbs.com.vn>
2022-08-21 09:44:13 +0700

    update code

--- src/test/java/runners/RunnerTest.java
+++ src/test/java/runners/RunnerTest.java
@@ -3,6 +3,7 @@ package runners;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import org.apache.logging.log4j.ThreadContext;
+import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.runner.RunWith;
 import utils.DriverManager;
@@ -39,16 +40,16 @@ public class RunnerTest {
         new DriverManager().initializeDriver();
     }
 
-//    @AfterClass
-//    public static void quit() {
-//        DriverManager driverManager = new DriverManager();
-//        if (driverManager.getDriver() != null) {
-//            driverManager.getDriver().quit();
-//            driverManager.setDriver(null);
-//        }
-//        ServerManager serverManager = new ServerManager();
-//        if (serverManager.getServer() != null) {
-//            serverManager.getServer().stop();
-//        }
-//    }
+    @AfterClass
+    public static void quit() {
+        DriverManager driverManager = new DriverManager();
+        if (driverManager.getDriver() != null) {
+            driverManager.getDriver().quit();
+            driverManager.setDriver(null);
+        }
+        ServerManager serverManager = new ServerManager();
+        if (serverManager.getServer() != null) {
+            serverManager.getServer().stop();
+        }
+    }
 }
